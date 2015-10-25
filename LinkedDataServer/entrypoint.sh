0f265e5751585027f94a43a3b162a01d87adb8d8
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2015-10-25 22:16:23 +0100

    not working yet

--- LinkedDataServer/entrypoint.sh
+++ LinkedDataServer/entrypoint.sh
@@ -2,10 +2,6 @@
 
 # This script should live in /LinkedDataServer/
 
-# cd /LinkedDataServer/
-
-# nohup google-refine-2.5/./refine &
-# nohup java -server -Xmx4g -jar blazegraph/bigdata-bundled.jar &
-
-cd jetty
-nohup java -jar start.jar jetty.port=8080 &
+nohup google-refine-2.5/./refine &
+nohup java -server -Xmx4g -jar blazegraph/bigdata-bundled.jar &
+jetty/./jetty.sh
